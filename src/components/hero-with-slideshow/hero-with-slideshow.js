582f1fcabf80604c41d90944c2939d2757784168
Raul Sanz <luar_znas@hotmail.com>
2022-12-06 17:03:05 +0000

    Update rounded background and media queries

--- src/components/hero-with-slideshow/hero-with-slideshow.js
+++ src/components/hero-with-slideshow/hero-with-slideshow.js
@@ -10,7 +10,7 @@ import Image3 from '../../images/img-slider-1.png'
 export default function HeroWithSlideshow() {
 
   return (
-    <div className="hero-with-slideshow__container">
+    <div className="hero-with-slideshow">
       <div className="container-fluid">
           <div className="hero-with-slideshow__text">
               <h2 className="hero-with-slideshow__text-heading">Our goal is the comfort and Safety of your pet during The trip</h2>
@@ -31,6 +31,15 @@ export default function HeroWithSlideshow() {
             </div>
           </Carousel>
       </div>
+      <svg width="500" height="80" viewBox="0 0 500 80" preserveAspectRatio="none">
+        <defs>
+          <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" stop-color="#FCEEDD" stop-opacity="1" />
+            <stop offset="100%" stop-color="#FFF8EF" stop-opacity="1" />
+          </linearGradient>
+        </defs>
+        <path d="M0,0 L0,40 Q250,80 500,40 L500,0 Z" fill="url(#grad1)" />
+      </svg>
     </div>
   )
 }
