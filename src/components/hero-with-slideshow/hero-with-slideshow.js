3a64c0bc5458ede7d4aab3dc9637a03b987ed62a
Raul Sanz <luar_znas@hotmail.com>
2022-12-06 18:39:19 +0000

    Change slider

--- src/components/hero-with-slideshow/hero-with-slideshow.js
+++ src/components/hero-with-slideshow/hero-with-slideshow.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
 import './hero-with-slideshow.scss'
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectCards } from "swiper";
+import "swiper/css";
+import "swiper/css/effect-cards";
+
 import Image1 from '../../images/img-slider-1.png'
-import Image2 from '../../images/img-slider-1.png'
-import Image3 from '../../images/img-slider-1.png'
+import Image2 from '../../images/img-slider-2.png'
+import Image3 from '../../images/img-slider-3.png'
 
 export default function HeroWithSlideshow() {
-
   return (
     <div className="hero-with-slideshow">
       <div className="container-fluid">
@@ -19,17 +21,11 @@ export default function HeroWithSlideshow() {
                 Contact us
               </a>
           </div>
-          <Carousel showThumbs={false} showStatus={false} showIndicators={false} showArrows={false} infiniteLoop={true} autoPlay={true} stopOnHover={true} centerMode={true} centerSlidePercentage={70} interval={7000} transitionTime={1000}>
-            <div>
-              <img src={Image1} />
-            </div>
-            <div>
-              <img src={Image2} />
-            </div>
-            <div>
-              <img src={Image3} />
-            </div>
-          </Carousel>
+          <Swiper autoplay={{ delay: 3000, disableOnInteraction: false }} effect={"cards"} grabCursor={true} modules={[Autoplay, EffectCards]} className="mySwiper">
+            <SwiperSlide><img src={Image1} /></SwiperSlide>
+            <SwiperSlide><img src={Image2} /></SwiperSlide>
+            <SwiperSlide><img src={Image3} /></SwiperSlide>
+          </Swiper>
       </div>
       <svg width="500" height="80" viewBox="0 0 500 80" preserveAspectRatio="none">
         <defs>
