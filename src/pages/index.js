90c56888e4b53b00c91dcb9739a63b95a9d00fdf
Raul Sanz <luar_znas@hotmail.com>
2022-12-08 16:26:17 +0000

    Build Text-Banner component

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import HeroWithSlideshow from '../components/hero-with-slideshow/hero-with-slide
 import ServicesBlock from '../components/services-block/services-block'
 import ArticlePreview from '../components/article-preview'
 import TextBlock from '../components/text-block/text-block'
+import TextBanner from '../components/text-banner/text-banner'
 
 import LogoDefra from '../images/logo-defra.png'
 
@@ -22,6 +23,7 @@ class RootIndex extends React.Component {
         <HeroWithSlideshow />
         <ServicesBlock />
         <ArticlePreview posts={posts} />
+        <TextBanner headingText="How to check a pet Passport?" bodyTexts={[ {text: "You can all the informations regarding pet passport in the PDF in the link below"} ]} buttonText="How to Complete Pet Passports.pdf" />
         <TextBlock id="about-us" headingText="About us" bodyTexts={[ {text: "Ohmydog Pet Travel: We are an international pet travel company specialized in fast and safe journeys between the United Kingdom, Ireland and Spain."}, {text: "We offer a luxury door-to-door service, with a reduced number of pets and 2 drivers with animal welfare trainning."}, {text: "Our vehicles are equipped with an advanced temperature control system and a GPS tracker. The vans have large bespoke crates and veterinary bedding for the comfort of the pets. All our vehicles are ATES and DEFRA approved."} ]} logo={LogoDefra} />
       </Layout>
     )
