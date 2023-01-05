ea7c1546c4b2b907b95be9bc90844e5ef24f686d
Raul Sanz <luar_znas@hotmail.com>
2023-01-05 14:24:10 +0000

    Change footer content and style

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,42 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import Language from './language'
 
-import Container from './container'
-import * as styles from './footer.module.css'
+import Logo from '../images/logo-footer.svg'
+
+import './footer.scss'
 
 const Footer = () => (
-  <Container as="footer">
-    <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
-    </div>
-  </Container>
+  <footer className='homepageFooter'>
+    <Link to="/" className="logoLink">
+      <img src={Logo} alt="Oh My Dog Logo" style={{width:'157px', height:'64px'}}/>
+    </Link>
+    <nav>
+      <ul className="navigation">
+        <li className="navigationItem">
+          <Link to="#services" activeClassName="active">
+            Services
+          </Link>
+        </li>
+        <li className="navigationItem">
+          <Link to="#blog" activeClassName="active">
+            Gallery
+          </Link>
+        </li>
+        <li className="navigationItem">
+          <Link to="#about-us" activeClassName="active">
+            About Us
+          </Link>
+        </li>
+        <li className="navigationItem contactus">
+          <Link to="#contact-us" activeClassName="active">
+            Contact Us
+          </Link>
+        </li>
+        <li className='navigationItem languageSelector'><Language /></li>
+      </ul>
+    </nav>
+  </footer>
 )
 
 export default Footer
