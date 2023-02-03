204f6e8f79032a30627b647b848b1f20b80d9afd
Raul Sanz <luar_znas@hotmail.com>
2023-02-03 13:24:14 +0000

    Update img to a variable size with StaticImage

--- src/components/hero-with-slideshow/hero-with-slideshow.js
+++ src/components/hero-with-slideshow/hero-with-slideshow.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import './hero-with-slideshow.scss'
 
+import { StaticImage } from "gatsby-plugin-image"
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectCards } from "swiper";
 import "swiper/css";
 import "swiper/css/effect-cards";
-
-import Image1 from '../../images/img-slider-1.webp'
-import Image2 from '../../images/img-slider-2.webp'
-import Image3 from '../../images/img-slider-3.webp'
 
 export default function HeroWithSlideshow(props) {
   return (
@@ -22,9 +19,9 @@ export default function HeroWithSlideshow(props) {
               </a>
           </div>
           <Swiper autoplay={{ delay: 3000, disableOnInteraction: false }} effect={"cards"} grabCursor={true} modules={[Autoplay, EffectCards]} className="mySwiper">
-            <SwiperSlide><img src={Image1} alt="Trip with a cat" width={790} height={810} /></SwiperSlide>
-            <SwiperSlide><img src={Image2} alt="Van's front view" width={1024} height={683} /></SwiperSlide>
-            <SwiperSlide><img src={Image3} alt="Trip collage" width={480} height={611} /></SwiperSlide>
+            <SwiperSlide><StaticImage alt="Trip with a cat" src="../../images/img-slider-1.webp" /></SwiperSlide>
+            <SwiperSlide><StaticImage alt="Van's front view" src="../../images/img-slider-2.webp" /></SwiperSlide>
+            <SwiperSlide><StaticImage alt="Trip collage" src="../../images/img-slider-3.webp" /></SwiperSlide>
           </Swiper>
       </div>
       <svg width="500" height="80" viewBox="0 0 500 80" preserveAspectRatio="none">
