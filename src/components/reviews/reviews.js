7e312fde5479905f426a233efd0b46707bd54503
Raul Sanz <luar_znas@hotmail.com>
2023-02-06 16:17:11 +0000

    CORS fix

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -16,7 +16,11 @@ function ReviewsPage ({headingText, reviewsText, api}) {
     let apiReturn = await axios({
       method: "get",
       url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=rating,reviews,user_ratings_total&key=${apiKey}&language=${locationLanguage}`,
-      headers: {}
+      headers: {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':'GET', 
+        'Content-Type': 'application/x-www-form-urlencoded'      
+      },
     })
     .then(async function(response) {
       return response;
