d617e4be36e2dec2f013b7cb6feb14f0229a654f
Raul Sanz <luar_znas@hotmail.com>
2023-07-28 09:07:20 +0100

    Update calendar files from contentful

--- src/components/text-banner/text-banner.js
+++ src/components/text-banner/text-banner.js
@@ -3,32 +3,26 @@ import './text-banner.scss'
 
 import Footprint from "../../images/footprint.svg"
 
-export default function TextBanner(props) {
-  return (
-    <div className='text-banner' id={props.id}>
+export default function TextBanner({id, color, headingText, year, descriptionText1, descriptionText2, pdf, buttonText, pdfCalendar}) {  return (
+    <div className='text-banner' id={id}>
       <div className='container-fluid'>
-          <div className='text-banner__text' id={props.color}>
+          <div className='text-banner__text' id={color}>
               <h2 className='text-banner__text-heading'>
-                {props.headingText}
-                {props.year &&
-                  <span>{props.year}</span>
+                {headingText}
+                {year &&
+                  <span>{year}</span>
                 }
               </h2>
               <p className='text-banner__text-description'>
-                {props.descriptionText1}
-                {props.year &&
-                  <span>{props.year}</span>
+                {descriptionText1}
+                {year &&
+                  <span>{year}</span>
                 }
-                {props.year &&
-                   <span>{props.descriptionText2}</span>
+                {year &&
+                   <span>{descriptionText2}</span>
                 }
               </p>
-              {props.pdf &&
-                <a className="button-secondary" href={props.pdf} target="_blank">{props.buttonText}</a>
-              }
-              {!props.pdf &&
-                <a className="button-secondary" href='/'>{props.buttonText}</a>
-              }
+              {pdf ? <a className="button-secondary" href={pdf} target="_blank">{buttonText}</a> : <a className="button-secondary" href={pdfCalendar} target="_blank">{buttonText}</a>}
               <div className='text-banner__background'>
                 <img src={Footprint} className='footprint-left' alt='Footprint background' />
                 <img src={Footprint} className='footprint-right' alt='Footprint background' />
