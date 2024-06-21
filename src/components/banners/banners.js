fd5488db3d1a2e1ac454e78d9ae39f53ac2edf97
Raul Sanz <sanzdiaz.raul@gmail.com>
2024-06-21 16:21:08 +0100

    Build new component for banners list

---
+++ src/components/banners/banners.js
@@ -0,0 +1,27 @@
+import React, { useState, useRef } from "react";
+import { GatsbyImage } from 'gatsby-plugin-image'
+import './banners.scss'
+
+const Banners = ({banners, padding, color}) => {
+
+  console.log('Banners', banners);
+  
+  return (
+    <div className={`banners ${ padding }`} style={{ background: color }}>
+      <div className='container-fluid'>
+          <ul className="banners-list">
+            {banners.map((banner, index) => (
+              <li className='navigationItem' key={index}>
+                <a href={banner.url} target="_blank" rel="noreferrer" aria-label="Instagram link">
+                  <GatsbyImage alt={banner.title} image={banner.image.gatsbyImage} key={index} />
+                  {banner.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Banners
