7a9c9f6e44602ae70c6398760d2cf1db2d659389
Raul Sanz <sanzdiaz.raul@gmail.com>
2024-07-30 19:59:37 +0100

    Update City Service pages and layout

--- src/templates/city-service-template.js
+++ src/templates/city-service-template.js
@@ -19,50 +19,48 @@ function withMyHook(Component) {
 }
 
 class CityTemplate extends React.Component {
+ 
   render() {
     const intl = this.props.intlValue;
 
     const city = get(this.props, 'data.contentfulCityServices');
-    const service = get(this.props, 'data.contentfulServices');
+    const serviceSlug = get(this.props, 'data.allContentfulServices.nodes[0].slug');
+
+    const title2 = intl.formatMessage({ id: 'cities.' + serviceSlug + '.title2' });
+    if (!title2) {
+      return null;
+    }
+
+    const title = intl.formatMessage({ id: 'cities.' + serviceSlug + '.meta-title1' });
+    const metaTitle = intl.formatMessage({ id: 'cities.' + serviceSlug + '.meta-title1' }) + city.cityName + intl.formatMessage({ id: 'cities.' + serviceSlug + '.meta-title2' });
+    const metaDescription = intl.formatMessage({ id: 'cities.' + serviceSlug + '.meta-description1' }) + city.cityName + intl.formatMessage({ id: 'cities.' + serviceSlug + '.meta-description2' });
+
     const bannersServices = get(this, 'props.data.banners_services.nodes');
-
-    let pathname = typeof window !== "undefined" ? window.location.pathname : "";
-    const locationLanguage = pathname.split("/")[1];
-
-    console.log('city.slug: ', city.slug);
-    //console.log('service.slug: ', service.slug);
-    
-    // const serviceSlugTranslation = (locationLanguage + '/' + city.slug + '-' + get(this.props, 'data.allContentfulServices.nodes[0].slug'))
-
-    // const seoTitle = intl.formatMessage({ id: "cities." + service.slug + ".meta-title1" }) + ' ' + city.cityName + ' ' + intl.formatMessage({ id: "cities." + service.slug + ".meta-title2" })  + ' ' + city.cityName;
-    // const seoDescription = intl.formatMessage({ id: "cities." + service.slug + ".meta-description1" }) + ' ' + city.cityName + ' ' + intl.formatMessage({ id: "cities." + service.slug + ".meta-description2" });
-
-    // console.log('city.slug: ', city);
-    // console.log('service.slug: ', service);
-
-    // console.log('MIX: ', (service.slug + ' - ' + city.cityName));
-
-    const seoTitle = "city.cityName";
-    const seoDescription = "city.cityName";
 
     return (
       <Layout location={this.props.location}>
         <Seo
-          title={seoTitle} 
-          description={seoDescription}
+          title={metaTitle} 
+          description={metaDescription}
         />
         <div className="city-service-template">
           <div className="container-fluid">
             <div className="city-service-template__details">
-              {/* <h1 className="city-service-template__details-title">{service.serviceName} {intl.formatMessage({ id: "cities.title-connector" })} {city.cityName}</h1> */}
+              {/* <span style={{fontStyle: 'italic'}}>
+                <h3 className="city-service-template__details-title">metaTitle: {metaTitle}</h3>
+                <h3 className="city-service-template__details-title">metaDescription: {metaDescription}</h3>
+              </span> */}
+              <h1 className="city-service-template__details-title">{title} {city.cityName}{title2}</h1>
             </div>
           </div>
         </div>
 
-        <Banners color="#ffffff" banners={bannersServices} />
-        {/* <TitleAndDescription color="#ffffff" padding="padding-bottom" descriptionText={intl.formatMessage({ id: "homepage.services.description2" })} /> */}
+        <TitleAndDescription color="#ffffff" descriptionText={metaDescription} />
+
+        <TitleAndDescription color="#ffffff" padding="padding-top" border="border-top" subheadingText={intl.formatMessage({ id: "homepage.services.title" }) + ' ' + intl.formatMessage({ id: "cities.title-connector" }) + ' ' + city.cityName} descriptionText={intl.formatMessage({ id: "homepage.services.description1" })} />
+        <Banners color="#ffffff" padding="padding-bottom" banners={bannersServices} />
         
-        <Contact subheadingText={intl.formatMessage({ id: "contact-us.title" })} descriptionText={intl.formatMessage({ id: "contact-us.description" })} formTitle={intl.formatMessage({ id: "city-page.contact-us.form-title" })} formDescription={intl.formatMessage({ id: "city-page.contact-us.form-description" })}openDays={intl.formatMessage({ id: "contact-us.open-days" })} openHours={intl.formatMessage({ id: "contact-us.open-hours" })} openDaysFriday={intl.formatMessage({ id: "contact-us.open-days-friday" })} openHoursFriday={intl.formatMessage({ id: "contact-us.open-hours-friday" })} closedDays={intl.formatMessage({ id: "contact-us.closed-days" })} closedText={intl.formatMessage({ id: "contact-us.closed-text" })} callText={intl.formatMessage({ id: "contact-us.call-text" })} telephone={intl.formatMessage({ id: "contact-us.phone" })} emailText={intl.formatMessage({ id: "contact-us.email-text" })} email={intl.formatMessage({ id: "contact-us.email" })} companyName={intl.formatMessage({ id: "contact-us.companyName" })} address={intl.formatMessage({ id: "contact-us.address" })} location={intl.formatMessage({ id: "contact-us.location" })} locationLinkText={intl.formatMessage({ id: "contact-us.locationLinkText" })} locationLink={intl.formatMessage({ id: "contact-us.locationLink" })} formNameText={intl.formatMessage({ id: "contact-us.user-name" })} formEmailText={intl.formatMessage({ id: "contact-us.user-email" })} formPhoneText={intl.formatMessage({ id: "contact-us.user-phone" })} formPickupCityText={intl.formatMessage({ id: "contact-us.user-pickup-city" })} formDropoffCityText={intl.formatMessage({ id: "contact-us.user-dropoff-city" })} formPetnumberText={intl.formatMessage({ id: "contact-us.user-pet-number" })} formPetinfoText={intl.formatMessage({ id: "contact-us.user-pet-info" })} formMessageText={intl.formatMessage({ id: "contact-us.user-message" })} buttonText={intl.formatMessage({ id: "contact-us.btn-text" })} />
+        <Contact subheadingText={intl.formatMessage({ id: "cities.contact-us.title" })} descriptionText={intl.formatMessage({ id: "contact-us.description" })} openDays={intl.formatMessage({ id: "contact-us.open-days" })} openHours={intl.formatMessage({ id: "contact-us.open-hours" })} openDaysFriday={intl.formatMessage({ id: "contact-us.open-days-friday" })} openHoursFriday={intl.formatMessage({ id: "contact-us.open-hours-friday" })} closedDays={intl.formatMessage({ id: "contact-us.closed-days" })} closedText={intl.formatMessage({ id: "contact-us.closed-text" })} callText={intl.formatMessage({ id: "contact-us.call-text" })} telephone={intl.formatMessage({ id: "contact-us.phone" })} emailText={intl.formatMessage({ id: "contact-us.email-text" })} email={intl.formatMessage({ id: "contact-us.email" })} companyName={intl.formatMessage({ id: "contact-us.companyName" })} address={intl.formatMessage({ id: "contact-us.address" })} location={intl.formatMessage({ id: "contact-us.location" })} locationLinkText={intl.formatMessage({ id: "contact-us.locationLinkText" })} locationLink={intl.formatMessage({ id: "contact-us.locationLink" })} formNameText={intl.formatMessage({ id: "contact-us.user-name" })} formEmailText={intl.formatMessage({ id: "contact-us.user-email" })} formPhoneText={intl.formatMessage({ id: "contact-us.user-phone" })} formPickupCityText={intl.formatMessage({ id: "contact-us.user-pickup-city" })} formDropoffCityText={intl.formatMessage({ id: "contact-us.user-dropoff-city" })} formPetnumberText={intl.formatMessage({ id: "contact-us.user-pet-number" })} formPetinfoText={intl.formatMessage({ id: "contact-us.user-pet-info" })} formMessageText={intl.formatMessage({ id: "contact-us.user-message" })} buttonText={intl.formatMessage({ id: "contact-us.btn-text" })} />
       </Layout>
     )
   }
@@ -74,7 +72,6 @@ export const pageQuery = graphql`
   query CityQuery(
     $slug: String
     $serviceSlug: String
-    $serviceSlugOrder: Int
     $language: String
   ) {
     contentfulCityServices(slug: { eq: $slug }, node_locale: { eq: $language }) {
@@ -91,7 +88,7 @@ export const pageQuery = graphql`
       slug
       id
     }
-    allContentfulServices(filter: { order: { eq: $serviceSlugOrder }, node_locale: { ne: $language } }) {
+    allContentfulServices(filter: {slug: {eq: $serviceSlug}, node_locale: { eq: $language } }) {
       nodes {
         slug
       }
