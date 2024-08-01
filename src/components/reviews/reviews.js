9ca0fddbdd13ee825461433d908aba2561779d8a
Raul Sanz <sanzdiaz.raul@gmail.com>
2024-08-01 16:14:34 +0100

    Update reviews page and block

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -4,14 +4,22 @@ import { Carousel } from 'react-responsive-carousel';
 
 import './reviews.scss';
 
-function ReviewsPage ({headingText, reviewsText, reviews, rating, userRating, padding, color, border}) { 
+function ReviewsPage ({headingText, subHeadingText, descriptionText, reviewsText, reviews, rating, userRating, padding, color, border}) { 
   return (
     <div id='reviews' className={`reviews ${ padding }`} style={{ background: color }}>
         <div className={`container-fluid ${ border }`} >
           <div className='reviews__heading'>
-            <h2 className='reviews__heading-title'>{headingText}</h2>
+            {headingText && 
+              <h1 className='reviews__heading-title'>{headingText}</h1>
+            }
+            {subHeadingText && 
+              <h2 className='reviews__heading-title'>{subHeadingText}</h2>
+            }
+            {descriptionText && 
+              <p className='reviews__text-description'>{descriptionText}</p>
+            }
             <div className='reviews__heading-rating'>
-              <p className='reviews__heading-rating'><span className='bold'>{rating}.0</span> <span className={`stars star-rating-${rating}`}></span> {userRating} {reviewsText}</p>
+              <p className='reviews__heading-rating'><span className='bold'>Rating: {rating}</span> <span className={`stars star-rating-${rating}`}></span> {userRating} {reviewsText}</p>
             </div>
           </div>
           <Carousel showArrows={true} showThumbs={false}>
